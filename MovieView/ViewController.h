fc3f6ca9a5d60d00769ec3aaa289953f8b289b61
Lenshen <779822644@qq.com>
2015-05-31 14:19:49 +0800

    added object
    added object

---
+++ MovieView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieView
+//
+//  Created by 鲍先生 on 15/5/31.
+//  Copyright (c) 2015年 鲍先生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MovieView
+//
+//  Created by 鲍先生 on 15/5/31.
+//  Copyright (c) 2015年 鲍先生. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
